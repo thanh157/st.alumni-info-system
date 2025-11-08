0bf05b2cde83fff0a24851b9bba4200b2f04a1c2
thanh157 <hd.thanh157@gmail.com>
2025-11-08 15:49:22 +0700

    fix-formm

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -1,7 +1,8 @@
 <!-- Footer -->
 <div class="navbar navbar-sm navbar-footer border-top">
-    <div class="container-fluid">
-        <span>&copy; 2024 <a href="">Duong Thanh</a></span>
-    </div>
+  <div class="container-fluid text-center py-2">
+    <span class="text-muted">&copy; 2025 - ST Team – Khoa Công nghệ Thông tin – Học viện Nông nghiệp Việt Nam</span>
+  </div>
 </div>
+
 <!-- /footer -->
