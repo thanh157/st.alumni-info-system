162283ac71413f8b2309541f094b4eb9a0e18efc
Trương Tuấn Hoàng <truongtuanhung09@gmail.com>
2025-11-16 21:43:33 +0700

    fix-ui-and-web-export-file

--- resources/views/admin/pages/admin/exports/report_tab2.blade.php
+++ resources/views/admin/pages/admin/exports/report_tab2.blade.php
@@ -1,15 +1,17 @@
 <table>
     <thead>
         <tr>
-            <th colspan="13" style="font-weight: bold; text-align: center; font-size: 14px;">HỌC VIỆN NÔNG NGHIỆP VIỆT
-                NAM</th>
+            <th colspan="4" style="text-align: left;font-size:14px; text-align:center;">
+                HỌC VIỆN NÔNG NGHIỆP VIỆT NAM</th>
         </tr>
         <tr>
-            <th colspan="13" style="font-weight: bold; text-align: center; font-size: 14px;">BAN QUẢN LÝ ĐÀO TẠO</th>
+            <th colspan="4"
+                style="text-align: left; font-weight:bold;text-align:center;font-size:14px;text-decoration: underline;">
+                KHOA: {{ $facultyName ?? '............' }}
+            </th>
         </tr>
         <tr>
-            <th colspan="13"
-                style="font-weight: bold; text-align: center; font-size: 16px; text-decoration: underline;">
+            <th colspan="13" style="font-weight: bold; text-align: center; font-size: 16px">
                 DANH SÁCH SINH VIÊN TỐT NGHIỆP NĂM {{ $schoolYear }}
             </th>
         </tr>
@@ -17,25 +19,29 @@
             <th colspan="13"></th>
         </tr>
         <tr style="font-weight: bold;">
+            <th rowspan="2" style="text-align: center; vertical-align: middle;">TT</th>
             <th rowspan="2" style="text-align: center; vertical-align: middle;">Mã sinh viên</th>
             <th rowspan="2" style="text-align: center; vertical-align: middle;">Họ và tên</th>
             <th rowspan="2" style="text-align: center; vertical-align: middle;">Nữ</th>
-            <th rowspan="2" style="text-align: center; vertical-align: middle;">Số thẻ CCCD/CMND</th>
-            <th rowspan="2" style="text-align: center; vertical-align: middle;">Mã ngành đào tạo<br>(Ghi bằng số theo mã
-                ngành tuyển sinh của Bộ Giáo dục và Đào tạo)</th>
+            <th rowspan="2" style="text-align: center; vertical-align: middle;">
+                Số thẻ CCCD
+            </th>
+            <th rowspan="2" style="text-align: center; vertical-align: middle;">Mã ngành đào tạo</th>
             <th colspan="2" style="text-align: center; vertical-align: middle;">Quyết định tốt nghiệp</th>
             <th colspan="2" style="text-align: center; vertical-align: middle;">Thông tin liên hệ</th>
             <th rowspan="2" style="text-align: center; vertical-align: middle;">Hình thức khảo sát<br>(Online, điện
-                thoại, email …)</th>
-            <th rowspan="2" style="text-align: center; vertical-align: middle;">Có phản hồi</th>
+                thoại, email, phỏng vấn, gửi tài liệu qua bưu điện…)</th>
+            <th rowspan="2" style="text-align: center; vertical-align: middle;">Có phản hồi<br>(Có phản hồi đánh dấu
+                X)</th>
+            <th rowspan="2" style="text-align: center; vertical-align: middle;">Ghi chú</th>
             <th rowspan="2" style="text-align: center; vertical-align: middle;">Ngành</th>
-            <th rowspan="2" style="text-align: center; vertical-align: middle;">Khóa học</th>
+            <th rowspan="2" style="text-align: center; vertical-align: middle;">Khoa</th>
         </tr>
         <tr style="font-weight: bold;">
             <th style="text-align: center; vertical-align: middle;">Số Quyết định</th>
             <th style="text-align: center; vertical-align: middle;">Ngày ký Quyết định</th>
-            <th style="text-align: center; vertical-align: middle;">Điện thoại</th>
-            <th style="text-align: center; vertical-align: middle;">Email</th>
+            <th style="text-align: center; vertical-align: middle;">Số điện thoại</th>
+            <th style="text-align: center; vertical-align: middle;">Email<br>(KHÔNG điền thông tin email của sinh viên do HVN cấp)</th>
         </tr>
     </thead>
     <tbody>
@@ -46,6 +52,7 @@
                 $major = $majors->get($item->training_industry_id);
             @endphp
             <tr>
+                <td>{{ $loop->iteration }}</td>
                 <td>{{ $item->code }}</td>
                 <td>{{ $item->full_name }}</td>
                 <td>{{ $item->gender == 'female' ? 'x' : '' }}</td>
@@ -56,8 +63,12 @@
                 </td>
                 <td>{{ $item->phone }}</td>
                 <td>{{ $item->email }}</td>
-                <td>{{ $res ? 'Có' : 'Không' }}</td>
+                <td>{{ $item->survey_method }}</td>
+                <td>{{ $res ? 'x' : '' }}</td>
+                <td>{{ $item->notes }}</td>
                 <td>{{ optional($major)->name }}</td>
+                <td>{{ $item->department }}</td>
+
             </tr>
         @empty
             <tr>
